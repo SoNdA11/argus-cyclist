84349175996b771b7ba9334cb82b973a97b90245
paulo sergio <psergio001192@gmail.com>
2026-01-20 18:49:00 -0300

    chore: update wails bindings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -122,3 +122,66 @@ export namespace domain {
 
 }
 
+export namespace main {
+	
+	export class ExportPoint {
+	    lat: number;
+	    lon: number;
+	    ele: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new ExportPoint(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.lat = source["lat"];
+	        this.lon = source["lon"];
+	        this.ele = source["ele"];
+	    }
+	}
+
+}
+
+export namespace storage {
+	
+	export class PowerRecord {
+	    duration: number;
+	    watts: number;
+	    wkg: number;
+	    // Go type: time
+	    date: any;
+	
+	    static createFrom(source: any = {}) {
+	        return new PowerRecord(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.duration = source["duration"];
+	        this.watts = source["watts"];
+	        this.wkg = source["wkg"];
+	        this.date = this.convertValues(source["date"], null);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
